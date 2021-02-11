97e23dd8d7aaef075922019b0ff5f014a4fe9257
Aleksey Kovalchuk <aleksey0koval@gmail.com>
2021-02-11 13:42:44 +0300

    Added task 10 module 4

---
+++ module4/task10/src/main/java/it/academy/Main.java
@@ -0,0 +1,22 @@
+package it.academy;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.stereotype.Service;
+
+import java.util.Arrays;
+
+@Service
+@ComponentScan(basePackages = "it.academy",
+        includeFilters = {@ComponentScan.Filter},
+        excludeFilters = {@ComponentScan.Filter})
+
+public class Main {
+    public static void main(String[] args) {
+        ApplicationContext context =
+                new AnnotationConfigApplicationContext(Main.class);
+        Arrays.stream(context.getBeanDefinitionNames()).forEach(System.out::println);
+        System.out.println(context.getBean(Person.class));
+    }
+}
